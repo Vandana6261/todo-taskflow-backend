a5b1fb8808e76bfb922b28beac6cd7a0c96fffdd
Vandana6261 <vvandanapatidar992@gmail.com>
2026-02-15 23:51:22 +0530

    feat: add and configure CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+const cors = require("cors")
 const express = require('express');
 const app = express();
 
+app.use(
+    cors({
+        origin: "http://localhost:5173"
+    })
+)
 app.use(express.json());
 
 app.use('/api/todo', require('./routes/todo.routes'));
