a8401f27bb29cb5f7b040c00d9e5a83510be02cd
Vandana6261 <vvandanapatidar992@gmail.com>
2026-02-21 12:24:07 +0530

    few tweaks

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todo.controllers')
     
-router.post('/', todoController.createTodo);
-router.get('/', todoController.getAllTodo);
+router.post('/', todoController.createTodo);    // done
+router.get('/', todoController.getAllTodo);     // done
 router.get('/:id', todoController.getTodo);
-router.delete('/:id', todoController.deleteTodo);
+router.delete('/:id', todoController.deleteTodo);   // done
 router.put('/:id', todoController.updateTodo);
 router.patch('/:id', todoController.updateTodoPartial);
 
